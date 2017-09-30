eab8117d150fd2392d301675c2b9505455dd5ba5
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-30 14:25:01 +0100

    added function to download from msdn site to text file.

--- AssemblesAndNameSpaces/Program.cs
+++ AssemblesAndNameSpaces/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 
 namespace AssemblesAndNameSpaces
 {
@@ -8,15 +9,17 @@ namespace AssemblesAndNameSpaces
         static void Main(string[] args)
         {
 
-
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
 
             // Example #2: Write one string to a text file.
-            string text = "we want to write this to our file.";
+            //string text = "we want to write this to our file.";
             // WriteAllText creates a file, writes the specified string to the file,
             // and then closes the file.    You do NOT need to call Flush() or Close().
-            System.IO.File.WriteAllText(@"F:\WriteText.txt", text);
+            System.IO.File.WriteAllText(@"F:\WriteText.txt", reply);
 
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(reply);
             Console.ReadLine();
         }
     }
