ee881ddf3994335867d828d049e43f43bdcab1a4
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-30 14:10:53 +0100

    write to text files using msdn code and system.io

--- AssemblesAndNameSpaces/Program.cs
+++ AssemblesAndNameSpaces/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace AssemblesAndNameSpaces
 {
@@ -10,6 +7,17 @@ namespace AssemblesAndNameSpaces
     {
         static void Main(string[] args)
         {
+
+
+
+            // Example #2: Write one string to a text file.
+            string text = "we want to write this to our file.";
+            // WriteAllText creates a file, writes the specified string to the file,
+            // and then closes the file.    You do NOT need to call Flush() or Close().
+            System.IO.File.WriteAllText(@"F:\WriteText.txt", text);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
